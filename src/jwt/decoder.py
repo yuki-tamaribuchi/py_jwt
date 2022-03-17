0cd230c80c62fab9b9fbc77903d42acf73240c08
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-03-17 10:26:44 +0900

    Change base64 from python standard lib base64 module to my_base64

--- src/jwt/decoder.py
+++ src/jwt/decoder.py
@@ -2,7 +2,7 @@ import hmac
 from hashlib import sha256
 from json import loads
 
-from base64 import b64decode, b64encode
+from py_base64.src.base64.base64 import base64encode, base64decode
 
 def jwt_decoder(token, secret_key):
 	if type(token) == str:
@@ -12,7 +12,7 @@ def jwt_decoder(token, secret_key):
 
 	header, payload, signature = token.split(b".")
 
-	verification_signature = b64encode(bytes(hmac.new(secret_key, header + b"." + payload, sha256).hexdigest(), "utf-8"))
+	verification_signature = base64encode(bytes(hmac.new(secret_key, header + b"." + payload, sha256).hexdigest(), "utf-8"))
 	
 	if signature != verification_signature:
 		print("Token verification error")
@@ -20,7 +20,7 @@ def jwt_decoder(token, secret_key):
 
 	decoded_token_dict = {}
 
-	decoded_token_dict["header"] = loads(b64decode(header))
-	decoded_token_dict["payload"]= loads(b64decode(payload))
+	decoded_token_dict["header"] = loads(base64decode(header))
+	decoded_token_dict["payload"]= loads(base64decode(payload))
 
 	return decoded_token_dict
